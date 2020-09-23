30f3260b6948cfa4f3465f0bed64c137165ac5e0
Rahul kumar <rahulojha2011@gmail.com>
2020-09-23 22:50:01 +0530

    register component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './home/register.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
     { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './home/register.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
